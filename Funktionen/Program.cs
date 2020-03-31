0e222200d5828282f98c0a1631ab195da234c229
Johannes B <johannes.bock@live.de>
2020-03-31 22:49:34 +0200

    Methoden und Variablen verstaendlicher benannt

--- Funktionen/Program.cs
+++ Funktionen/Program.cs
@@ -8,21 +8,21 @@ namespace Funktionen
         {
             Console.WriteLine("Punkte:");
             Punkt p1 = new Punkt();
-            Console.WriteLine("p1: x="+p1.x+" y="+p1.y);
-            p1.x = 5;
-            p1.y = 3;
-            Console.WriteLine("p1: x=" + p1.x + " y=" + p1.y);
+            Console.WriteLine("p1: x="+p1.xKoordinate + " y="+p1.yKoordinate);
+            p1.xKoordinate = 5;
+            p1.yKoordinate = 3;
+            Console.WriteLine("p1: x=" + p1.xKoordinate + " y=" + p1.yKoordinate);
             Console.WriteLine(p1.ToString());
             Punkt p2 = new Punkt(5, 4);
-            Console.WriteLine("p2: x=" + p2.x + " y=" + p2.y);
+            Console.WriteLine("p2: x=" + p2.xKoordinate + " y=" + p2.yKoordinate);
             LineareFunktion f1 = new LineareFunktion(2, 1);
             Console.WriteLine("f1: " + f1.ToString());
-            Console.WriteLine("Nullstelle von f1: "+f1.Nullstelle());
-            Console.WriteLine("Schnittpunkt mit x-Achse von f1: "+f1.SchnittpunktXAchse().ToString());
-            Console.WriteLine("Schnittpunkt mit y-Achse von f1: "+f1.SchnittpunktYAchse().ToString());
+            Console.WriteLine("Nullstelle von f1: "+f1.NullstelleBerechnen());
+            Console.WriteLine("Schnittpunkt mit x-Achse von f1: "+f1.SchnittpunktXAchseBerechnen().ToString());
+            Console.WriteLine("Schnittpunkt mit y-Achse von f1: "+f1.SchnittpunktYAchseBerechnen().ToString());
             LineareFunktion f2= new LineareFunktion(2,2);
             Console.WriteLine("f2: " + f2.ToString());
-            Console.WriteLine("f2(0)=" + f2.Funktionswert(0));
+            Console.WriteLine("f2(0)=" + f2.FunktionswertBestimmen(0));
             if (VielleichtSchnittpunktVonFunktionenBestimmen(f1, f2) == null)
             {
                 Console.WriteLine("Es gibt keinen Schnittpunkt.");
@@ -34,23 +34,23 @@ namespace Funktionen
             Punkt p3 = new Punkt();
             LineareFunktion f3 = new LineareFunktion(p1, p3);
             Console.WriteLine("f3: " + f3.ToString());
-            Console.WriteLine(LageVonFunktionen(f1,f2));
+            Console.WriteLine(LageVonFunktionenBestimmen(f1,f2));
 
         }
 
         static Punkt SchnittpunktVonFunktionenBerechnen(LineareFunktion f, LineareFunktion g)
         {
-            Punkt SP = new Punkt();            
-            SP.x = (g.n - f.n) / (f.m - g.m);
-            SP.y = f.Funktionswert(SP.x);
-            return SP;            
+            Punkt schnittPunkt = new Punkt();
+            schnittPunkt.xKoordinate = (g.yAchsenabschnitt - f.yAchsenabschnitt) / (f.anstieg - g.anstieg);
+            schnittPunkt.yKoordinate = f.FunktionswertBestimmen(schnittPunkt.xKoordinate);
+            return schnittPunkt;            
         }
 
         static Punkt? VielleichtSchnittpunktVonFunktionenBestimmen(LineareFunktion f, LineareFunktion g)
         {
-            if (f.m == g.m)
+            if (f.anstieg == g.anstieg)
             {
-                if (f.n == g.n)
+                if (f.yAchsenabschnitt == g.yAchsenabschnitt)
                 {                    
                     return null;
                 }
@@ -66,11 +66,11 @@ namespace Funktionen
         }
 
 
-        static string LageVonFunktionen(LineareFunktion f, LineareFunktion g)
+        static string LageVonFunktionenBestimmen(LineareFunktion f, LineareFunktion g)
         {
-            if (f.m == g.m)
+            if (f.anstieg == g.anstieg)
             {
-                if (f.n == g.n)
+                if (f.yAchsenabschnitt == g.yAchsenabschnitt)
                 {
                     return "Die Funktionen sind identisch.";
                 }
@@ -89,26 +89,26 @@ namespace Funktionen
     public class Punkt
     {
         //Eigenschaften (oeffentlich oder privat)
-        public double x { get; set; }
-        public double y { get; set; }
+        public double xKoordinate { get; set; }
+        public double yKoordinate { get; set; }
 
         //Konstruktor
         public Punkt()
         {
-            x = 0;
-            y = 0;
+            xKoordinate = 0;
+            yKoordinate = 0;
         }
         public Punkt(double x0, double y0)
         {
-            x = x0;
-            y = y0;
+            xKoordinate = x0;
+            yKoordinate = y0;
         }
 
         //Methoden 
         public override string ToString()
         {
             string k;
-            k = "x:" + x.ToString() + ", y:" + y.ToString();
+            k = "x:" + xKoordinate.ToString() + ", y:" + yKoordinate.ToString();
             return k;
         }
     }
@@ -118,63 +118,63 @@ namespace Funktionen
         //y=f(x)=m*x+n
 
         //Eigenschaften
-        public double m { get; set; }
-        public double n { get; set; }
+        public double anstieg { get; set; }
+        public double yAchsenabschnitt { get; set; }
 
         //Konstruktor
         public LineareFunktion()
         {
-            m = 1;
-            n = 0;
+            anstieg = 1;
+            yAchsenabschnitt = 0;
         }
 
         public LineareFunktion(double m0, double n0)
         {
-            m = m0;
-            n = n0;
+            anstieg = m0;
+            yAchsenabschnitt = n0;
         }
 
         public LineareFunktion(Punkt p1, Punkt p2)
         {
-            m = (p2.y - p1.y) / (p2.x - p1.x);
-            n = p2.y - m * p2.x;
+            anstieg = (p2.yKoordinate - p1.yKoordinate) / (p2.xKoordinate - p1.xKoordinate);
+            yAchsenabschnitt = p2.yKoordinate - anstieg * p2.xKoordinate;
         }
 
         //Methoden
         public override string ToString()
         {
             string s;
-            s = "y=f(x)=" + m + "x+" + n;
+            s = "y=f(x)=" + anstieg + "x+" + yAchsenabschnitt;
             return s;
         }
 
-        public double Funktionswert(double x0)
+        public double FunktionswertBestimmen(double x0)
         {
             double fw;
-            fw = m * x0 + n;
+            fw = anstieg * x0 + yAchsenabschnitt;
             return fw;
         }
 
-        public double Nullstelle()
+        public double NullstelleBerechnen()
         {
             double nst;
-            nst = -n / m;
+            nst = -yAchsenabschnitt / anstieg;
             return nst;
         }
 
-        public Punkt SchnittpunktXAchse()
+        public Punkt SchnittpunktXAchseBerechnen()
         {
             Punkt SPx = new Punkt();
-            SPx.y = 0;
-            SPx.x = Nullstelle();
+            SPx.yKoordinate = 0;
+            SPx.xKoordinate = NullstelleBerechnen();
             return SPx;
         }
 
-        public Punkt SchnittpunktYAchse()
+        public Punkt SchnittpunktYAchseBerechnen()
         {
             Punkt SPy = new Punkt();
-            SPy.x = 0;
-            SPy.y = n;
+            SPy.xKoordinate = 0;
+            SPy.yKoordinate = yAchsenabschnitt;
             return SPy;
         }
 
